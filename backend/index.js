dc49d9be27a98d5bd683073b9cc71ac74cb86b28
FranciscoCTI <franciscofcch@gmail.com>
2025-04-07 13:37:15 -0400

    #Midu solution/ Agregar header Access-Control-Allow-Origin.

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
     app.get(/(.*)/, (req, res) => {
+        res.header('Access-Control-Allow-Origin', '*');
         res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
     })
 }
